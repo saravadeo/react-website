8de3d0bc5245c75b454526fae838d34a9ed52c0a
Onkar Sarvade <saravadeo@yahoo.com>
2026-04-18 13:19:47 +0000

    Deploy website with geek theme and blog section
    - Add built files to docs/ folder for GitHub Pages
    - Update .gitignore to exclude build artifacts
    - Website now deployed from master/docs folder

---
+++ docs/precache-manifest.fbf9c4a6d0d4126a6e1409c5535c8cd1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681467df389f7ffe280c3d3028d2093e",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "d57c0ea8b17fc2548d78",
+    "url": "/react-website/static/css/main.e13155a2.chunk.css"
+  },
+  {
+    "revision": "2bd4a6328ffd8efe4474",
+    "url": "/react-website/static/js/2.e38b6e71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-website/static/js/2.e38b6e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d57c0ea8b17fc2548d78",
+    "url": "/react-website/static/js/main.dd47ea5e.chunk.js"
+  },
+  {
+    "revision": "e2c5d39c4a3edd236ec9",
+    "url": "/react-website/static/js/runtime-main.13098694.js"
+  },
+  {
+    "revision": "84eb90b2ae58cb77504a97086d44ca81",
+    "url": "/react-website/static/media/hero.84eb90b2.jpg"
+  },
+  {
+    "revision": "8d67c4e2d3ff7dbb099bae19910c06c5",
+    "url": "/react-website/static/media/nat-10.8d67c4e2.jpg"
+  },
+  {
+    "revision": "3a801913ac4a5a2ce277846b5628d033",
+    "url": "/react-website/static/media/nat-4.3a801913.jpg"
+  },
+  {
+    "revision": "74b6c5ab79f5db053d6dcfd07ec9e704",
+    "url": "/react-website/static/media/nat-5.74b6c5ab.jpg"
+  },
+  {
+    "revision": "755697f642b098ae396b9c8c1a94253a",
+    "url": "/react-website/static/media/nat-6.755697f6.jpg"
+  },
+  {
+    "revision": "a803e5dcfdff53e4471008f03fc5a35f",
+    "url": "/react-website/static/media/nat-7.a803e5dc.jpg"
+  },
+  {
+    "revision": "5ed8880c5d06cd9c4b3ff992988824ed",
+    "url": "/react-website/static/media/nat-8.5ed8880c.jpg"
+  },
+  {
+    "revision": "cd6054cf3980be00debb3ca5af493861",
+    "url": "/react-website/static/media/nat-81.cd6054cf.jpg"
+  },
+  {
+    "revision": "d405808780239e38bcdcd6b080c47378",
+    "url": "/react-website/static/media/nat-9.d4058087.jpg"
+  },
+  {
+    "revision": "04e15372b96db0b9c6a38880da0df457",
+    "url": "/react-website/static/media/nat-91.04e15372.jpg"
+  },
+  {
+    "revision": "a6e5c0d2275461002c8ec4d8d3563497",
+    "url": "/react-website/static/media/video.a6e5c0d2.webm"
+  },
+  {
+    "revision": "c67a14feb28938ff9e6ddc2f2dee212f",
+    "url": "/react-website/static/media/video.c67a14fe.mp4"
+  }
+]);
